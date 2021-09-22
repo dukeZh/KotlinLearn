fa8e2d43f7b5fcaa9514b1c36a7b647851e40d4a
zhaihai <duke_zh@yeah.net>
2021-09-22 14:38:49 +0800

    init 0919 kotlin 集合操作

--- myLibrary/src/main/java/com/example/mylibrary/Kotlin0920.kt
+++ myLibrary/src/main/java/com/example/mylibrary/Kotlin0920.kt
@@ -1,6 +1,7 @@
 package com.example.mylibrary
 
 import android.os.Build
+import java.io.File
 
 //59->  0920 学习记录
 fun main() {
@@ -54,18 +55,77 @@ fun main() {
 //        println("$index,$item")
 //    }
 
-    //练习 用; 拼接集合中得数据
-    val mutableList = mutableListOf("Json", "Jack", "Jacky")
-    var imagesUrls = ""
-    mutableList.forEachIndexed { index, item ->
-//        val item2 = item.takeIf { index < mutableList.size - 1 }
-//        val let = item2?.let { item.plus(";") }
-//        imagesUrls = imagesUrls.plus(let?:item)
+    //练习 用; 拼接List集合中得数据
+//    val mutableList = mutableListOf("Json", "Jack", "Jacky")
+//    var imagesUrls = ""
+//    mutableList.forEachIndexed { index, item ->
+////        val item2 = item.takeIf { index < mutableList.size - 1 }
+////        val let = item2?.let { item.plus(";") }
+////        imagesUrls = imagesUrls.plus(let?:item)
+//
+//        imagesUrls = imagesUrls.plus(item.takeIf { index < mutableList.size - 1 }
+//            ?.plus(";") ?: item)
+//    }
+//    println(imagesUrls)
 
-        imagesUrls = imagesUrls.plus(item.takeIf { index < mutableList.size - 1 }
-            ?.plus(";") ?: item)
-    }
-    println(imagesUrls)
+//    //解构 一次性通过集合给多个元素赋值
+//    val list = listOf("Json", "Jack", "Jacky")
+//
+//    //"_" 表示不需要这个值，省略
+//    val (origin,_,proxy) = list
 
+    //Set 创建与元素获取（元素不可重复）
+//    val set = setOf("Json", "Jack", "Jacky", "Json")
+//    println(set.elementAt(2))
+
+    //可变set集合
+//    val mutableSet = mutableSetOf("Json", "Jack", "Jacky", "Json")
+//    mutableSet += "Jimmy"
+
+    //集合转换与快捷函数
+//    val list = listOf("Json", "Jack", "Jacky","Jack")
+//        .toSet()
+//        .toList()
+//    println(list)
+
+    //distinct 去重
+//    println(listOf("Json", "Jack", "Jacky","Jack").distinct())
+
+    //数组类型  引用类型，可以编译成java基本数据类型
+//    val intArray = intArrayOf(10, 30, 40)
+//    listOf(10,30,40).toIntArray()
+//    val arrayOf = arrayOf(File("xxx"), File("yyy"))
+
+    //map 的创建 (元素不可以重复，重复及覆盖)
+//    val mapOf = mapOf("Jack" to 20, "Jason" to 18, "Jack" to 30)
+//
+//    val mapOf1 = mapOf(Pair("Jimmy", 20), Pair("Jack", 20))
+
+    //读取map的值
+//    val map = mapOf("Jack" to 20, "Jason" to 18, "Jack" to 30)
+//    println(map["Jack"])
+//    println(map.getValue("Jack"))
+//    println(map.getOrElse("Rose"){"Unknown"})
+//    println(map.getOrDefault("Rose",0))
+
+    //遍历map
+//    val map = mapOf("Jack" to 20, "Jason" to 18, "Jack" to 30)
+//    map.forEach{
+//        println("${it.key},${it.value}")
+//    }
+//    map.forEach{ (key:String,value:Int) ->
+//        println("$key,$value")
+//    }
+
+    //可变map 集合
+    val mutableMapOf = mutableMapOf("Jack" to 20, "Jason" to 18, "Jack" to 30)
+    mutableMapOf += "Jimmy" to 30
+
+    //等价于map.put()
+   // mutableMapOf["Jimmy"] = 31
+
+    //添加Rose，如果没有就添加
+   // mutableMapOf.getOrPut("Rose"){ 18 }
+    println(mutableMapOf)
 
 }
